7a5e2cf90bc0e48b09e10a6dc837e3b1b875e05e
lonikefaes <156308287+lonikefaes@users.noreply.github.com>
2024-07-22 14:32:55 +0200

    Delete nordic_analyses directory

--- nordic_analyses/add_scatter_boxchart.m
+++
@@ -1,8 +0,0 @@
-clc;clear all; close all
-n = 10;mu = [0 1 2];% artificial data here
-X = randn(n,3) + mu;
-figure();bp = boxchart(X);hold all;bp.WhiskerLineColor = [1,1,1];% do figure
-for it = 1:n
-    plot(bp.XData,bp.YData(it,:),'.','MarkerSize',20); % no connecting lines
-    plot(bp.XData,bp.YData(it,:),'.-','MarkerSize',20);% lines   
-end
--- nordic_analyses/partitionSSols.m
+++
@@ -1,5 +0,0 @@
-function [TSS,SSb,SSe] = partitionSSols(Y,X,beta)
-TSS = sum( (Y - mean(Y) ).^2  ); % total sum of squares
-e   = Y - X*beta;                % residuals
-SSb = sum( (X*beta - mean(X*beta)).^2  );
-SSe = sum( (e - mean(e) ).^2  );
